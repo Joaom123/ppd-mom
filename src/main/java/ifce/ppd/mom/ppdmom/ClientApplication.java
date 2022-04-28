968f97dc69a525cc2aa8219f0fd4defabdaf6b0a
João Marcus <joaomarcus12769@gmail.com>
2022-04-28 17:47:17 -0300

    add jar

--- src/main/java/ifce/ppd/mom/ppdmom/ClientApplication.java
+++ src/main/java/ifce/ppd/mom/ppdmom/ClientApplication.java
@@ -17,7 +17,7 @@ public class ClientApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("client-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 890, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("IFCE - João Marcus Maia Rocha - PPD - MOM");
         stage.setScene(scene);
         stage.show();
     }
